8c7e0273e7b567cce6da1203face0847f4cef81e
JW-kim-k <tyty321321@gmail.com>
2022-08-10 20:13:28 +0900

    save string data as korean to fix views.py

--- erooming/api/views.py
+++ erooming/api/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.parsers import JSONParser
 from api.models import Users, Routine, User_Routine
 from api.serializer import UsersSerializer, RoutineSerializer, UserRoutineSerializer
-
+import json
 
 def users_list(request):
     if request.method == "GET":
@@ -23,8 +23,8 @@ def routine_list(request):
     if request.method == "GET":
         routine  = Routine.objects.all()
         routineSerializer = RoutineSerializer(routine, many=True)
-        return JsonResponse(routineSerializer.data, safe=False)
-    
+        return JsonResponse(routineSerializer.data,json_dumps_params={"ensure_ascii":False},safe=False)
+
     elif request.method == "POST":
         data = JSONParser().parse(request)
         userSerializer = RoutineSerializer(data=data)
