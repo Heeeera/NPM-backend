9c55d2cdd64d1de447502f826949f90f65d710a6
JW-kim-k <tyty321321@gmail.com>
2022-08-10 17:02:18 +0900

    add users_list and routine_list function to views.py

--- erooming/api/views.py
+++ erooming/api/views.py
@@ -1,3 +1,37 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from rest_framework.parsers import JSONParser
+from api.models import Users, Routine, User_Routine
+from api.serializer import UsersSerializer, RoutineSerializer, UserRoutineSerializer
+
+
+def users_list(request):
+    if request.method == "GET":
+        users = Users.objects.all()
+        userSerializer = UsersSerializer(users, many=True)
+        return JsonResponse(userSerializer.data, safe=False)
+    
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        userSerializer = UsersSerializer(data=data)
+        if userSerializer.is_valid():
+            userSerializer.save()
+            return JsonResponse(userSerializer.data, status=201)
+        return JsonResponse(userSerializer.errors, status=400)
+
+def routine_list(request):
+    if request.method == "GET":
+        routine  = Routine.objects.all()
+        routineSerializer = RoutineSerializer(routine, many=True)
+        return JsonResponse(routineSerializer.data, safe=False)
+    
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        userSerializer = RoutineSerializer(data=data)
+        if userSerializer.is_valid():
+            userSerializer.save()
+            return JsonResponse(userSerializer.data, status=201)
+        return JsonResponse(userSerializer.errors, status=400)
+
 
 # Create your views here.
