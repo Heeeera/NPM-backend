612989ca492c8c90ad81344a569d1d8d2bc41ecc
JW-kim-k <tyty321321@gmail.com>
2022-08-10 16:58:38 +0900

    add models ( Users, Routine, UserRoutine ) to models.py in api

--- erooming/api/models.py
+++ erooming/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+class Users(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+
+class Routine(models.Model):
+    title = models.CharField(max_length=100)
+    max_people_number = models.IntegerField()
+    now_people_number = models.IntegerField()
+    description = models.CharField(max_length=200)
+    start_date = models.IntegerField()
+    end_date = models.IntegerField()
+    max_count = models.IntegerField()
+    status = models.CharField(max_length=10)
+
+class User_Routine(models.Model):
+    user_id = models.IntegerField()
+    routine_id = models.IntegerField()
+    now_count = models.IntegerField()
+    max_count = models.IntegerField()
+    is_host = models.BooleanField()
+
+
 # Create your models here.
