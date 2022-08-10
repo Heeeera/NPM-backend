fba6646f5fc84d4dba0ff701514470d451cbb907
JW-kim-k <tyty321321@gmail.com>
2022-08-10 17:10:15 +0900

    add users and routine url

--- erooming/erooming/urls.py
+++ erooming/erooming/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api.views import user_lists, routine_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/',include('users_lists')),
+    path('routine/',include('routine_list')),
 ]
