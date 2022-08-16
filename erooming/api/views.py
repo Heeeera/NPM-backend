09ea2019521632776a41a1f95a45eeacbe34c519
Heeeera <chr5487@naver.com>
2022-08-16 14:27:10 +0900

    Delete user model

--- erooming/api/views.py
+++ erooming/api/views.py
@@ -1,30 +1,10 @@
-from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
-from api.models import User, Routine, User_Routine
-from api.serializer import UserSerializer, RoutineSerializer, UserRoutineSerializer
+from api.models import Routine, User_Routine
+from api.serializer import RoutineSerializer, UserRoutineSerializer
 from django.views.decorators.csrf import csrf_exempt
-from django.db.models import Q
-
-@api_view(['GET', 'POST'])
-def users_list(request):
-    if request.method == "GET":
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-
-    elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = UserSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=400)
-
 
 @csrf_exempt
 @api_view(['GET', 'POST'])
